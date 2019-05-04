5ab6ceb9c6c31c77361eb7ebe2f31a14f548c005
dabin <ggdabin@gmail.com>
2019-05-04 15:35:35 +0800

    Create 2.sh

---
+++ 2.sh
@@ -0,0 +1,11 @@
+#on cornet server
+export PATH=$PATH:/usr/sbin:/usr/bin
+iptables -I INPUT -p udp -j ACCEPT
+iptables -I INPUT -p tcp --dport 36687 -j ACCEPT
+iptables -I INPUT -p tcp -s 42.200.198.191 --dport 8118 -j ACCEPT
+iptables -I INPUT -p tcp --dport 22 -j ACCEPT
+iptables-save | perl -ne '$H{$_}++ or print' | iptables-restore
+cd /root/shadowsocks
+nohup python server.py -c config.json &
+cd /tmp/
+nohup /root/httpproxy &
